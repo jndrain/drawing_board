8d31c2c693cb3aa2593685dd8d5d98ca00b2568d
Daniel Thompson <danthwa@gmail.com>
2016-10-28 10:07:38 -0700

    installed and configured bootstrap

--- db/migrate/20161026231033_add_attachment_avatar_to_artists.rb
+++ db/migrate/20161026231033_add_attachment_avatar_to_artists.rb
@@ -1,12 +1,12 @@
 class AddAttachmentAvatarToArtists < ActiveRecord::Migration
-  def self.up
-  	add_attachment :artists, :avatar
-    change_table :artists do |t|
-      t.attachment :avatar
-    end
-  end
-
-  def self.down
-    remove_attachment :artists, :avatar
-  end
+  # def self.up
+  # 	add_attachment :artists, :avatar
+  #   change_table :artists do |t|
+  #     t.attachment :avatar
+  #   end
+  # end
+  #
+  # def self.down
+  #   remove_attachment :artists, :avatar
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,12 +22,8 @@ ActiveRecord::Schema.define(version: 20161027232557) do
     t.string   "password_digest"
     t.string   "city"
     t.string   "state"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "collabs", force: :cascade do |t|
@@ -56,12 +52,8 @@ ActiveRecord::Schema.define(version: 20161027232557) do
     t.text     "description"
     t.string   "media"
     t.integer  "artist_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "projects", ["artist_id"], name: "index_projects_on_artist_id"
